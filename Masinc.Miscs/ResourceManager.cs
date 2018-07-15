c0dc9c5db072a299cd480fca18882ca23826410e
masinc <masinc000@gmail.com>
2018-07-15 14:21:49 +0900

    ResourceManagerクラスの作成

---
+++ Masinc.Miscs/ResourceManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Masinc.Miscs
+{
+    public sealed class ResourceManager : IDisposable
+    {
+        readonly List<IDisposable> resource;
+        bool disposed;
+        readonly object disposed_lock;
+
+        public ResourceManager()
+        {
+            resource = new List<IDisposable>();
+            disposed_lock = new object();
+        }
+
+        public void Add(IDisposable disposable)
+        {
+            resource.Add(disposable);
+        }
+
+        ~ResourceManager()
+        {
+            lock (disposed_lock)
+            {
+                if (!disposed)
+                {
+                    Dispose(false);
+                }
+            }
+        }
+
+        public void Dispose()
+        {
+            lock (disposed_lock)
+            {
+                if (!disposed)
+                {
+                    Dispose(true);
+                }
+            }
+        }
+
+        void Dispose(bool disposing)
+        {
+            foreach (var d in resource)
+            {
+                d.Dispose();
+            }
+            this.disposed = true;
+        }
+    }
+
+    public static partial class Misc
+    {
+        public static TDisposable AddResourceManager<TDisposable>(TDisposable @this, ResourceManager resourceManager)
+            where TDisposable : IDisposable
+        {
+            resourceManager.Add(@this);
+            return @this;
+        }
+    }
+}
